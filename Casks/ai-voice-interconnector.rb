dd7cdce0d29bd4669bba8271f79fe3d0f8cbc9d2
ai-voice-interconnector-ci <ci@ai-voice-interconnector.invalid>
2026-09-22 01:07:38 +0000

    Cask: actualizar a v0.20.1

--- Casks/ai-voice-interconnector.rb
+++ Casks/ai-voice-interconnector.rb
@@ -1,6 +1,6 @@
 cask "ai-voice-interconnector" do
-  version "0.20.0"
-  sha256 "a10a222d658d50a2b4a217b503aa0d7b485dbf3164ee39db855f704a83f28e1e"
+  version "0.20.1"
+  sha256 "3665ff39de0a93f708f990af0dce8d238607db5a1af0c38299c32876da291b7d"
 
   url "https://github.com/CristianRojas-SoftwareEngineer/AI-Voice-InterConnector/releases/download/v#{version}/ai-voice-interconnector-#{version}-arm64-macos.tar.gz"
   name "AI Voice InterConnector"
